56622c053821cee74399a577355641535e80982e
ckartik7 <96309750+ckartik7@users.noreply.github.com>
2023-09-03 21:41:13 +0530

    Add files via upload

---
+++ task-1/script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu= document.querySelector(".sm-menu-links");
+    const icon= document.querySelector(".sm-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
